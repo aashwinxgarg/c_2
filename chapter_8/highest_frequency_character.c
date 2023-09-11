38ad95165a0a648bfc1d305b943ba74380a53c3a
aashwingarg7679 <aashwingarg7679@gmail.com>
2023-09-11 18:09:22 +0530

    updating chapter_8

--- chapter_8/highest_frequency_character.c
+++ chapter_8/highest_frequency_character.c
@@ -4,6 +4,21 @@
 
 int main()
 {
-    
+    char str[]="aashwin garg",c;
+    int count=0;
+    char coun[100];
+    for(int i=0;str[i]!='\0';i++)
+    {
+        count=0;
+        for(c='a';c<='z';c++)
+        {
+            if(c==str[i])
+            {
+                count++;
+            }
+        }
+        coun[i]=count;
+    }
+    //printf("%d",coun[20]);
     return 0;
 }
